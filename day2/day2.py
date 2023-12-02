b3e8dccbfddbfb64efdb2c29b52fb57341ed745a
Lucas Laurent <lucas.laurent@telecomnancy.eu>
2023-12-02 14:33:42 +0100

    day2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -2,18 +2,18 @@ def main():
     # On veut lire le fichier input.txt
     # On veut lire chaque ligne du fichier
     with open("input.txt", "r") as f:
-        lines = f.readlines()
-        lines = [line.strip() for line in lines]
-        lines = [line[line.index(": ") + 2:] for line in lines]
-        lines = [line.split("; ") for line in lines]
-        lines = [[y.split(", ") for y in x] for x in lines]
-        lines = [[[z.split(" ") for z in y] for y in x] for x in lines]
+        games = f.readlines()
+        games = [line.strip() for line in games]
+        games = [line[line.index(": ") + 2:] for line in games]
+        games = [line.split("; ") for line in games]
+        games = [[y.split(", ") for y in x] for x in games]
+        games = [[[z.split(" ") for z in y] for y in x] for x in games]
 
-    part1(lines)
+    part1(games)
+    part2(games)
 
 
 def part1(games):
-
     redcubes = 12
     greencubes = 13
     bluecubes = 14
@@ -36,9 +36,30 @@ def part1(games):
             if not valid:
                 break
         if valid:
-            result += i+1
+            result += i + 1
 
     print("Part1: " + str(result))
+
+
+def part2(games):
+
+    result = 0
+
+    for i in range(len(games)):
+        redcubes = 0
+        greencubes = 0
+        bluecubes = 0
+        for sets in games[i]:
+            for cube in sets:
+                if cube[1] == "red" and redcubes != max(int(cube[0]), redcubes):
+                    redcubes = max(int(cube[0]), redcubes)
+                if cube[1] == "blue" and bluecubes != max(int(cube[0]), bluecubes):
+                    bluecubes = max(int(cube[0]), bluecubes)
+                if cube[1] == "green" and greencubes != max(int(cube[0]), greencubes):
+                    greencubes = max(int(cube[0]), greencubes)
+        result += redcubes * greencubes * bluecubes
+
+    print("Part2: " + str(result))
 
 
 if __name__ == "__main__":
