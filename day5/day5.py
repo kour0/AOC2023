c5df8787c06c5870e278eeb090b3d7337ded9a2a
Lucas Laurent <lucas.laurent@telecomnancy.eu>
2023-12-07 02:35:29 +0100

    day5

--- day5/day5.py
+++ day5/day5.py
@@ -41,6 +41,7 @@ def position(maps, k, number):
         number = inmap(maps, i, number)
     return number
 
+
 def part1(maps, seed, k_map):
     lower_destination = inf
     for number in seed:
@@ -49,10 +50,35 @@ def part1(maps, seed, k_map):
     print("Part 1: ", lower_destination)
 
 
-def part2(maps, seed, k_map):
-    lower_destination = inf
-    for k in range(0, len(seed), 2):
+def min_range(maps, k, min, max):
+    k = str(k)
+    for i in range(len(maps[k])):
+        min_source = int(maps[k][i][1])
+        max_source = int(maps[k][i][1]) + int(maps[k][i][2]) - 1
+        delta = int(maps[k][i][0]) - int(maps[k][i][1])
+        if min_source <= min <= max_source and min_source <= max <= max_source:
+            return [(min + delta, max + delta)]
+        elif min_source <= min <= max_source < max:
+            return [(min + delta, max_source + delta)] + min_range(maps, k, max_source + 1, max)
+        elif max_source >= max >= min_source > min:
+            return min_range(maps, k, min, min_source - 1) + [(min_source + delta, max + delta)]
+    return [(min, max)]
 
+
+def part2(maps, seed, k_map):
+    time_start = time.time()
+    lower_destination = inf
+    list_interval = [(seed[i], seed[i] + seed[i+1] - 1) for i in range(0, len(seed), 2)]
+    dict_list_interval = dict()
+    dict_list_interval["-1"] = list_interval
+    for k in range(k_map + 1):
+        dict_list_interval[str(k)] = []
+        for interval in dict_list_interval[str(k-1)]:
+            dict_list_interval[str(k)] += min_range(maps, k, interval[0], interval[1])
+        if k == k_map:
+            lower_destination = min(dict_list_interval[str(k)])[0]
+    print("Time: ", time.time() - time_start)
+    print("Part 2: ", lower_destination)
 
 
 if __name__ == "__main__":
