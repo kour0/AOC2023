4a1975cc4d5202b49aa51175a4e2ae1cd4ecd281
Lucas Laurent <114945778+kour0@users.noreply.github.com>
2023-12-09 18:22:23 +0100

    day7 part2

--- day7/day7.py
+++ day7/day7.py
@@ -7,8 +7,8 @@ def main():
         games = [line.strip() for line in games]
         games = [line.split(" ") for line in games]
 
-    print(games)
     part1(games)
+    part2(games)
 
 
 def type_hand(hand):
@@ -43,13 +43,10 @@ def part1(games):
     rank = 0
 
     for key in dict_games.keys():
-        # if 2 cards have same value, sort by order cards list, if the first card is the same, sort by second card, etc...
-        # dict_games[key] = [(cards, bid), (cards, bid), (cards, bid), (cards, bid), (cards, bid)] where cards is 5 cards
-        print(key + ": " + str(dict_games[key]))
+        # sort the differnts hands by the cards, the egalities are sorted by the rank of the hand
         dict_games[key] = sorted(dict_games[key], key=lambda x: (
             cards.index(x[0][0]), cards.index(x[0][1]), cards.index(x[0][2]), cards.index(x[0][3]),
             cards.index(x[0][4])))
-        print(dict_games[key])
         for i in range(len(dict_games[key])):
             rank += 1
             result += rank * int(dict_games[key][i][1])
@@ -57,8 +54,93 @@ def part1(games):
     print("Part1: " + str(result))
 
 
+def change_joker_bis(hand):
+    typ_han = type_hand(hand)
+
+    if typ_han == "Flush":
+        return hand.replace("J", "A")
+    elif typ_han == "Four of a kind":
+        # replace the joker by the card that is in the hand 4 times
+        return hand.replace("J", hand[hand.index("J") - 1])
+    elif typ_han == "Full house":
+        # replace the joker by the card that is in the hand 3 times
+        for card in hand:
+            if hand.count(card) == 3 and card != "J":
+                return hand.replace("J", card)
+            elif hand.count(card) == 2 and card != "J":
+                return hand.replace("J", card)
+    elif typ_han == "Three of a kind":
+        # replace the joker by the card that is in the hand 3 times
+        for card in hand:
+            if hand.count(card) == 3 and card != "J":
+                return hand.replace("J", card)
+        return hand.replace("J", "A")
+    elif typ_han == "Two pairs":
+        # replace the joker by the best card that is in the hand 2 times
+        best_card = ""
+        for card in hand:
+            if hand.count(card) == 2 and card != "J":
+                best_card += card
+        return hand.replace("J", max(best_card, key=lambda x: cards.index(x)))
+    elif typ_han == "One pair":
+        # replace the joker by the card that is in the hand 2 times
+        for card in hand:
+            if hand.count(card) == 2 and card != "J":
+                return hand.replace("J", card)
+        return hand.replace("J", "A")
+    else:
+        # replace the joker by the best card
+        return hand.replace("J", "A")
+
+
+def change_joker(hand):
+    ensemble = set(hand)
+    max_occ = 0
+    max_card = ""
+    for card in ensemble:
+        if card != "J":
+            if max_occ == hand.count(card):
+                max_card = max(max_card, card, key=lambda x: cards.index(x))
+            elif hand.count(card) > max_occ:
+                max_occ = hand.count(card)
+                max_card = card
+    if max_occ != 0:
+        return hand.replace("J", max_card)
+    else:
+        return hand.replace("J", "A")
+
+
 def part2(games):
-    pass
+    result = 0
+
+    dict_games = dict()
+    dict_games["High card"] = []
+    dict_games["One pair"] = []
+    dict_games["Two pairs"] = []
+    dict_games["Three of a kind"] = []
+    dict_games["Full house"] = []
+    dict_games["Four of a kind"] = []
+    dict_games["Flush"] = []
+
+    for game in games:
+        if "J" in game[0]:
+            new_game = change_joker(game[0])
+            dict_games[type_hand(new_game)] += [[new_game, game[1]]]
+        else:
+            dict_games[type_hand(game[0])] += [game]
+
+    rank = 0
+
+    for key in dict_games.keys():
+        dict_games[key] = sorted(dict_games[key], key=lambda x: (
+            cards.index(x[0][0]), cards.index(x[0][1]), cards.index(x[0][2]), cards.index(x[0][3]),
+            cards.index(x[0][4])))
+        print(key + " : " + str(dict_games[key]))
+        for i in range(len(dict_games[key])):
+            rank += 1
+            result += rank * int(dict_games[key][i][1])
+
+    print("Part2: " + str(result))
 
 
 if __name__ == "__main__":
